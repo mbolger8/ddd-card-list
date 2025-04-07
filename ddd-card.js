1f8ff85407dee1b817bdac275980053e02520004
mbolger8 <mjbolger462@gmail.com>
2025-04-06 20:08:01 -0400

    commits

--- ddd-card.js
+++ ddd-card.js
@@ -20,16 +20,10 @@ export class DddCard extends DDDSuper(I18NMixin(LitElement)) {
 
   constructor() {
     super();
-    this.image = "https://via.placeholder.com/150";
-    this.title = "Card List";
-    this.description = "Description";
-    this.button = "Button";
-    this.buttonLink = "#";
+    this.image = "";
+    this.title = "";
+    this.description = "";
     this.primary = "";
-    this.t = {
-      ...this.t,
-      title: "Title",
-    };
   }
 
   // Lit reactive properties
@@ -40,8 +34,7 @@ export class DddCard extends DDDSuper(I18NMixin(LitElement)) {
       description: { type: String },
       link: { type: String },
       image: { type: String },
-      button: { type: String },
-      buttonLink: { type: String },
+      link: { type: String },
       primary: {type: String, reflect: true, attribute: "data-primary"},
     };
   }
@@ -52,31 +45,64 @@ export class DddCard extends DDDSuper(I18NMixin(LitElement)) {
     css`
       :host {
         display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
+        border: 1px solid var(--ddd-border-color, #ccc);
+        border-radius: var(--ddd-border-radius, 12px);
+        width: var(--ddd-card-width, 400px);
+        font-family: var(--ddd-font-family, Arial, sans-serif);
+        background-color: var(--ddd-background-color, #fff);
       }
       .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
+        display: flex;
+        flex-wrap: wrap;
+        gap: 20px;
+        border-radius: var(--ddd-border-radius, 12px);
       }
       h3 span {
         font-size: var(--ddd-card-list-label-font-size, var(--ddd-font-size-s));
       }
       .card {
-        height: var(--ddd-card-height, 300px);
-        width: var(--ddd-card-width, 200px);
+        width: var(--ddd-card-width, 100%);
+      }
+      .img img{
+        width: 100%;
+        object-fit: cover;
+      }
+      .title {
+        text-align: inherit;
+        font-size: 1.375rem;
+        font-family: Roboto, Arial, Tahoma, sans-serif;
+        font-weight: 700;
+        line-height: 120%;
+        letter-spacing: normal;
+        color: var(--theme-ui-colors-nittanyNavy);
+      }
+      .button {
         display: flex;
-        flex-direction: column;
-        background-color: var(--ddd-theme-accent);
-        border-radius: var(--ddd-border-radius-m);
-        box-shadow: var(--ddd-shadow-1);
+        justify-content: center;
+        align-items: center;
+        border-radius: var(--ddd-border-radius, 4px);
+        color: var(--theme-ui-colors-white);
+        background-color: var(--ddd-background-color, #005fa9);
+        padding-top: 0.75rem;
+        padding-bottom: 0.75rem;
+        padding-left: 1.5rem;
+        padding-right: 1rem;
+      }
+      .description {
+        text-align: inherit;
+        font-size: 1.125rem;
+        font-family: Arial, Tahoma, sans-serif;
+        font-weight: 400;
+        line-height: 150%;
+        letter-spacing: normal;
+        color: var(--theme-ui-colors-coalyGray);
+        margin-bottom: 0rem;
       }
     `];
   }
 
-  handleButtonClick() {
-    window.open(this.buttonLink, "_blank");
+  clickEvent() {
+    window.open(this.link);
   }
 
   // Lit render the HTML
@@ -94,7 +120,7 @@ export class DddCard extends DDDSuper(I18NMixin(LitElement)) {
             <slot></slot>
         </div>
         <div class="button">
-            <button @click="${this.handleButtonClick}">${this.button}</button>
+          <button @click=${this.clickEvent}>Explore ></button>
         </div>
     </div>
 </div>`;
